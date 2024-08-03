3a00e20089b94aa6f65f42fc0612ea2bf931ed3c
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-03 13:31:01 +0000

    Update yolo_inference.py

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO 
 
-model = YOLO('models\yolov8_trained_best_model.pt')
+model = YOLO('models/yolov8_trained_best_model.pt')
 
-results = model.predict('input videos//1.mp4',save=True)
+results = model.predict('input videos/1.mp4',save=True)
 print(results[0])
 print('=====================================')
 for box in results[0].boxes:
