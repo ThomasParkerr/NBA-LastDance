6ffb7af8a5f80a2aec752b61d8835393b43c6d6b
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-03 13:29:56 +0000

    Add files via upload

---
+++ yolo_inference.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO 
+
+model = YOLO('models\yolov8_trained_best_model.pt')
+
+results = model.predict('input videos//1.mp4',save=True)
+print(results[0])
+print('=====================================')
+for box in results[0].boxes:
+    print(box)
