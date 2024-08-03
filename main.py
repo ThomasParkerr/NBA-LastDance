6df3a48cbe6816904b98b615e023414f1d964741
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-03 18:12:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import numpy as np
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
-from speed_and_distance_estimator import MetricsEstimator
 from speed_and_distance_estimator import SpeedAndDistance_Estimator
 
 
