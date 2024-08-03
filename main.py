61df683ecd824f581e6dea2dca229f4c4e992b5f
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-03 14:05:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utils import read_video, save_video
 
 def main():
     # Read Video
-    video_frames = read_video('input_videos/08fd33_4.mp4')
+    video_frames = read_video('input videos/1.mp4')
 
     # Save video
     save_video(video_frames, 'output_videos/output_video.avi')
