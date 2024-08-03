ceeed83050a8a9a28a61579e2ec1671903674b6d
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-03 14:03:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
     # Save video
-    save_video(output_video_frames, 'output_videos/output_video.avi')
+    save_video(video_frames, 'output_videos/output_video.avi')
 
 if __name__ == '__main__':
     main()
