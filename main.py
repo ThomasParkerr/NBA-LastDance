41bf6bacdc762b25335796f35277f52de1b4fc7d
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-03 15:27:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,21 @@ def main():
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
 
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+
+    
+
     #for track_id, player in tracks['players'][0].items():
        # bbox = player['bbox']
         #frame = video_frames[0]
