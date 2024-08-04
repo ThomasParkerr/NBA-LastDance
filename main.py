ab7608421701191711e8e215700ec2967ca37e11
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-04 12:30:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -43,13 +43,13 @@ def main():
     team_assigner.assign_team_color(video_frames[0], 
                                     tracks['players'][0])
     
-    for frame_num, player_track in enumerate(tracks['players']):
-        for player_id, track in player_track.items():
-            team = team_assigner.get_player_team(video_frames[frame_num],   
-                                                 track['bbox'],
-                                                 player_id)
-            tracks['players'][frame_num][player_id]['team'] = team 
-            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+    #for frame_num, player_track in enumerate(tracks['players']):
+     #   for player_id, track in player_track.items():
+      #      team = team_assigner.get_player_team(video_frames[frame_num],   
+       #                                          track['bbox'],
+        #                                         player_id)
+         #   tracks['players'][frame_num][player_id]['team'] = team 
+          #  tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
     # Assign Ball Acquisition
     # player_assigner = PlayerBallAssigner()
