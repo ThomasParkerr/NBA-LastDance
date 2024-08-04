e15dfeb9b7f534890cc63d3f0f96f53c7f24d9fb
ThomasParkerr <145340119+ThomasParkerr@users.noreply.github.com>
2024-08-04 16:16:56 +0000

    Update video_utils.py

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -26,3 +26,10 @@ def read_video(video_path):
         print(f"Warning: No frames were read from {video_path}")
     
     return frames
+
+def save_video(output_video_frames, output_video_path):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+    for frame in output_video_frames:
+        out.write(frame)
+    out.release()
